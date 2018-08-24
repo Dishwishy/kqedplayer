3d5cdea8abe12c4b19ef59e39359a00aab513d10
Kyle Champlin <kchamplin@splunk.com>
2018-08-24 14:16:35 -0700

    Initial Commit

---
+++ KQEDRADIO/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KQEDRADIO
+//
+//  Created by Kyle Champlin on 8/24/18.
+//  Copyright © 2018 Kyle Champlin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KQEDRADIO/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KQEDRADIO
+//
+//  Created by Kyle Champlin on 8/24/18.
+//  Copyright © 2018 Kyle Champlin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
